ce43c3af6c7cb9aab02bd89dd59e1b8f7d77c10a
Matt Rodrigues <68609506+Matheus-Rodrigues-EC@users.noreply.github.com>
2023-09-17 12:57:42 -0300

    Feat: configured graphic responsivity to change with days

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -128,7 +128,7 @@ function Home(){
         console.log("Geolocation is not supported by this browser.");
         }
 
-    }, [city, colorbackground, colortext]);
+    }, [city, colorbackground, colortext, days]);
 
     return (
         <Container>
